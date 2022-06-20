3d3a3477d2ef39986bdfc7212efeac9772761734
Gabriel Sens <gabriel_sens@hotmail.com>
2022-06-19 23:20:15 -0300

    criando e definindo header do mycontacts

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    background: '#F6F5FC',
+    primary: {
+      lighter: '#e0e3ff',
+      light: '#6647f4',
+      main: '#5061FC',
+      dark: '#3346f0',
+    },
+  },
+};
