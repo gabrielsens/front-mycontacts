a4b35b826552cf04a2e939c256e0392064a59f3c
Gabriel Sens <gabriel_sens@hotmail.com>
2023-02-16 23:02:35 -0300

    routes imperative

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import NewContact from './Pages/NewContact';
 import EditContact from './Pages/EditContact';
 
 export default function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<NewContact />} />
-      <Route path="/edit/:id" element={<EditContact />} />
-    </Routes>
-  );
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/new', element: <NewContact /> },
+    { path: '/edit/:id', element: <EditContact /> },
+  ]);
+  return routes;
 }
